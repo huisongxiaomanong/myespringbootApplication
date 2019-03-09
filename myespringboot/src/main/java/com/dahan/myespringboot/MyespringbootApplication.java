a535684a542b65ad7e02d3c8c9f4e07c1f7eaa21
huisongxiaomanong <976226535@qq.com>
2019-03-09 16:31:11 +0800

    提交本地

---
+++ myespringboot/src/main/java/com/dahan/myespringboot/MyespringbootApplication.java
@@ -0,0 +1,13 @@
+package com.dahan.myespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyespringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyespringbootApplication.class, args);
+	}
+
+}
